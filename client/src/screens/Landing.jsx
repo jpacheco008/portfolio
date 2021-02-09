2fecd55c63ef1b4671f026e957252df26f94083d
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-09 18:31:22 -0500

    format

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -1,14 +1,14 @@
-import '../styles/Landing.css'
+import "../styles/Landing.css";
 import { Link } from "react-router-dom";
 
 export default function Landing() {
   return (
-    <div className='landing-container'>
-      <h1 className='landing-name'>Julio Pacheco</h1>
-      <h5 className='landing-title'>Software Engineer/Electrical Engineer</h5>
-      <Link to='about-me' className='landing-button-link'>
-       <button className='landing-button'>GET TO KNOW ME</button>
+    <div className="landing-container">
+      <h1 className="landing-name">Julio Pacheco</h1>
+      <h5 className="landing-title">Software Engineer/Electrical Engineer</h5>
+      <Link to="about-me" className="landing-button-link">
+        <button className="landing-button">GET TO KNOW ME</button>
       </Link>
     </div>
-  )
+  );
 }
