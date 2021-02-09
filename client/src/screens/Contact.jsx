cb1b62166b8d10bba439bdb04b23a7df06481f4e
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-02-09 18:23:04 -0500

    contact

--- client/src/screens/Contact.jsx
+++ client/src/screens/Contact.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import "../styles/Contact.css";
 
 export default function Contact() {
   return (
-    <div>
-      
+    <div className="contact-container">
+      <h2 className="contact-text">Let's Create Together</h2>
+      <div className="contact-links">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/jpacheco008"
+        >
+          <img
+            src="https://image.flaticon.com/icons/png/512/25/25231.png"
+            alt="github"
+            className="contact-icon"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/julio-pacheco-06958bb7/"
+        >
+          <img
+            src="https://www.vectorico.com/wp-content/uploads/2018/02/LinkedIn-Icon-Squircle-Dark.png"
+            alt="linkedin"
+            className="contact-icon"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="mailto:jpacheco0825@gmail.com"
+        >
+          <img
+            src="https://i.pinimg.com/originals/bb/18/bd/bb18bdbbef437b2d50518db5a8292c94.png"
+            alt="email"
+            className="contact-icon"
+          />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
