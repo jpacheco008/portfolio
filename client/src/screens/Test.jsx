6c7576123fae9b85a9e2e074b76e93961fabc112
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-04-15 10:01:23 -0400

    remove test screen

--- client/src/screens/Test.jsx
+++
@@ -1,9 +0,0 @@
-export default function Test() {
-  return (
-    <div className="lines">
-    <div className="line1"></div>
-    <div className="line2"></div>
-    <div className="line3"></div>
-  </div>
-  );
-}
