e5de2437e0df2d761eac37ad4b843178b11e88b1
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-05-04 18:16:44 -0400

    changed icons

--- client/src/components/Project5.jsx
+++ client/src/components/Project5.jsx
@@ -1,5 +1,4 @@
 import DoItRight from "../assets/Do_It_Right.jpg";
-import webLogo from "../assets/icons8-website-64.png";
 
 export default function Project3() {
   return (
@@ -25,7 +24,7 @@ export default function Project3() {
           href="https://chrome.google.com/webstore/detail/do-it-right/fologgfgadjcdjemgkohffpbbcegmipb"
         >
           <p>To live site</p>
-          <img src={webLogo} alt="live-site" className="project-logo" />{" "}
+          <i class="fas fa-globe fa-2x"></i>
         </a>
         <a
           target="_blank"
@@ -34,11 +33,7 @@ export default function Project3() {
           href="https://github.com/Rest4-Hackathon/Small-Biz-Rest"
         >
           <p>To Github Repo</p>
-          <img
-            src="https://image.flaticon.com/icons/png/512/25/25231.png"
-            alt="github"
-            className="project-logo"
-          />
+          <i className="fab fa-github fa-2x"></i>
         </a>
       </div>
     </div>
